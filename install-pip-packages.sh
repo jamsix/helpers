482fa559652a6f8a01f579eb31df465c4055ec1e
P <p@jam.si>
2015-12-29 11:39:05 +0100

    Config on top

--- install-pip-packages.sh
+++ install-pip-packages.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+packages=('itsdangerous-0.24' 'MarkupSafe-0.23' 'Jinja2-2.8' 'Werkzeug-0.11.3' 'Flask-0.10.1' 'SQLAlchemy-1.0.11' 'Flask-SQLAlchemy-2.0')
+
+
 PIP_OK=$(dpkg-query -W --showformat='${Status}\n' python-pip | grep "install ok installed")
 if [ "" == "$PIP_OK" ]; then
   apt-get --force-yes --yes install python-pip python-dev build-essential
 fi
-
-packages=('itsdangerous-0.24' 'MarkupSafe-0.23' 'Jinja2-2.8' 'Werkzeug-0.11.3' 'Flask-0.10.1' 'SQLAlchemy-1.0.11' 'Flask-SQLAlchemy-2.0')
 
 for i in "${packages[@]}"
 do
